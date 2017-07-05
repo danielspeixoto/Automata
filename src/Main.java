29b37f1d8e12d7a7f7b5bad84663a08b5a143435
Daniel <danielspeixoto@outlook.com>
2017-07-04 22:38:26 -0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,18 @@ public class Main {
         for(int i = 0; i < numOfStates; i++) {
             states.add(new State(statesNames[i]));
         }
+
         // Não é necessário
         scanner.nextInt();
         scanner.nextLine();
         scanner.nextLine();
+
+        int numOfFinals = scanner.nextInt();
+        scanner.nextLine();
+        String[] finalNames = scanner.nextLine().split(",");
+        for(int i = 0; i < numOfFinals; i++) {
+            searchForState(finalNames[i]).setFinal(true);
+        }
 
         int numOfRules = scanner.nextInt();
         scanner.nextLine();
@@ -29,16 +37,15 @@ public class Main {
                     .addRule(rule[1].charAt(0), searchForState(rule[2]));
         }
 
-        //TODO Identificar estados finais e inicial
-        searchForState("01").setFinal(true);
-
         String answer = "REJEITA";
         State current = states.get(0);
         for(char letter : scanner.nextLine().toCharArray()) {
-            System.out.println(current.getName());
+            // Apagar comentario pra ver caminho percorrido
+            // System.out.println(current.getName());
             current = current.read(letter);
         }
-        System.out.println(current.getName());
+        // Apagar comentario pra ver caminho percorrido
+        // System.out.println(current.getName());
         if(current.isFinal()) {
             answer = "ACEITA";
         }
